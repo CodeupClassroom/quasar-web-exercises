f3690df4b2ceddc192588970811d85b6168e8b5e
kennethhowell <howell.kenneth.a@gmail.com>
2021-08-12 14:39:51 -0500

    complete: javascript w/ html lecture

---
+++ js/external.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("Hello from External JS!");
+var myStr = 45;
+
+// console.log(myStr)
+
+
+// alert("Hey, this is my alert :)")
+// var userConfirm = confirm("Do you want to remain logged in?")
+//
+// alert("The user selected: " + userConfirm)
+
+var userLunch = prompt("What did you have for lunch?")
+
+alert("The user had " + userLunch + " for lunch!")
