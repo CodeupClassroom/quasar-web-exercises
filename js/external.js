f80d5be0f141b454ef75354af9f9dc60660aee6f
kennethhowell <howell.kenneth.a@gmail.com>
2021-08-16 09:08:59 -0500

    friday: exercise reviews for function.js and intro to Js w/ html

--- js/external.js
+++ js/external.js
@@ -1,16 +1,24 @@
-"use strict";
-
-console.log("Hello from External JS!");
-var myStr = 45;
-
-// console.log(myStr)
-
-
-// alert("Hey, this is my alert :)")
-// var userConfirm = confirm("Do you want to remain logged in?")
+// "use strict";
 //
-// alert("The user selected: " + userConfirm)
+// console.log("Hello from External JS!");
+// var myStr = 45;
+//
+// // console.log(myStr)
+//
+//
+// // alert("Hey, this is my alert :)")
+// // var userConfirm = confirm("Do you want to remain logged in?")
+// //
+// // alert("The user selected: " + userConfirm)
+//
+// var userLunch = prompt("What did you have for lunch?")
+//
+// alert("The user had " + userLunch + " for lunch!")
 
-var userLunch = prompt("What did you have for lunch?")
+// console.log("This is external JS, please leave me alone");
 
-alert("The user had " + userLunch + " for lunch!")
+// alert("Welcome to my Website!");
+//
+// var userFavorite = prompt("Hey, what's up! What's your favorite color?");
+//
+// alert(userFavorite + "? Wow! That is EXACTLY MY FAVORITE TOO!");
