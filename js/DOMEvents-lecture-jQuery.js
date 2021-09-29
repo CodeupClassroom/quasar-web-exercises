4c77521bc515f8eb0acf55737d1bddefa2ef4677
kennethhowell <howell.kenneth.a@gmail.com>
2021-09-29 10:08:10 -0500

    complete: key events lecture

--- js/DOMEvents-lecture-jQuery.js
+++ js/DOMEvents-lecture-jQuery.js
@@ -1,6 +1,9 @@
 var button1 = $("#btn1");
 var button2 = $("#btn2");
 
+//keyboard global
+var myKeys = "";
+
 //click event
 button1.click(function (e) {
     alert("Button 1 clicked");
@@ -14,10 +17,31 @@ button2.dblclick(function () {
     alert("Button 2 double clicked")
 })
 
-//hover even
+//hover event
 $("p").hover(function () {
-    $("p").css("background-color", "rebeccapurple")
-},
-    function(){
+        $("p").css("background-color", "rebeccapurple")
+    },
+    function () {
         $("p").css("background-color", "inherit")
     })
+
+//keydown
+// $("body").keydown(function (event){
+//     console.log(event.key);
+// })
+
+//keypress
+// $("body").keypress(function (event){
+//     console.log(event.key);
+// })
+
+//keyup
+$("body").keyup(function (event){
+    console.log("The key you pressed was: "+ event.key);
+    myKeys += event.key;
+    console.log(myKeys);
+    //if my user types 'quasar'> do a cool thing
+    if(myKeys === "quasar"){
+        alert("coooooool thing 8)")
+    }
+})
